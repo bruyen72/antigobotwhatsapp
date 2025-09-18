65b39a385d1435322c15203870de3e62f303e505
Bruno <bruyen72@gmail.com>
2025-09-18 15:21:42 -0400

    descrição da atualização

--- replit.nix
+++ replit.nix
@@ -1,5 +1,20 @@
 { pkgs }: {
   deps = [
+    pkgs.alsa-lib
+    pkgs.alsa-lib-with-plugins
+    pkgs.systemdUkify
+    pkgs.systemd
+    pkgs.python313Packages.systemd
+    pkgs.pangolin
+    pkgs.pango
+    pkgs.cairo-lang
+    pkgs.cairo
+    pkgs.gnomeExtensions.bring-out-submenu-of-power-offlogout-button
+    pkgs.libgbm
+    pkgs.wl-clipboard-x11
+    pkgs.libloragw-sx1301
+    pkgs.xorg.libX11
+    pkgs.libxkbcommon_8
     pkgs.nodejs_20
     pkgs.chromium
     pkgs.libuuid
@@ -21,5 +36,10 @@
     pkgs.xorg.libXrender
     pkgs.xorg.libXtst
     pkgs.xorg.libxcb
+    # Adicionando as dependências faltantes:
+    pkgs.stdenv.cc.cc.lib  # Para libstdc++.so.6
+    pkgs.xorg.libXcursor   # Para libXcursor.so.1
+    pkgs.xorg.libXi        # Para libXi.so.6
+    pkgs.gdk-pixbuf        # Para libgdk_pixbuf-2.0.so.0
   ];
 }
